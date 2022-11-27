1ad1dfb9272f2a88fb4e4315ca210798cdecd009
MrB <mrblasco@gmail.com>
2022-11-27 09:45:43 +0100

    documentation

--- R/formats.R
+++ R/formats.R
@@ -1,4 +1,4 @@
-#' Output formats for the JRC reportss
+#' Output formats for the JRC reports
 #'
 #' Each function is a wrapper for \code{\link[bookdown]{pdf_document2}} to
 #' produce documents in JRC style.
@@ -18,14 +18,26 @@ jrc_tech_report <- function(...) {
 	bookdown::pdf_document2(..., template=template)
 }
 
+#' Render R markdown file for the JRC reports
+#'
+#' This function is a wrapper for \code{\link[rmarkdown]{render}} to
+#' produce documents in JRC technical reports style.
+#'
+#' @param input path to R markdown file.
+#' @param \dots Arguments passed to \code{\link[rmarkdown]{render}}.
+#'
+#' @return An R Markdown output format object.
+#'
+#' @author Andrea Blasco
 #' @export
-render_tech_report <- function(input, output_format=jrc_tech_report(), ...) {
+render_tech_report <- function(input, ...) {
 	skeleton <- system.file("rmarkdown/templates/report/skeleton"
 		, package = "jrcTemplates")
 	outpath <- file.path(dirname(input), "skeleton")
 	dir.create(outpath, showWarnings=FALSE)
-	file.copy(from = list.files(skeleton, full=T), to = outpath, overwrite=TRUE)	
-	out <- rmarkdown::render(input, output_format=output_format, ...)
+	file.copy(from = list.files(skeleton, full=T), to = outpath
+		, overwrite=TRUE)	
+	out <- rmarkdown::render(input, output_format=jrc_tech_report(), ...)
 	unlink(outpath, recursive=TRUE)	
 	invisible(out)
 }
